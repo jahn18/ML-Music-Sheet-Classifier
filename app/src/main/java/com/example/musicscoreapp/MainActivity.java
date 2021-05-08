bfa29ac9faca5b9d02fe5fc8a08f583a19336b37
Jefferson <jefferson@zhai.ca>
2021-05-07 19:47:41 -0700

    preemptive code for a POST request containing an image

--- app/src/main/java/com/example/musicscoreapp/MainActivity.java
+++ app/src/main/java/com/example/musicscoreapp/MainActivity.java
@@ -14,7 +14,9 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,12 +25,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -37,6 +49,8 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,12 +68,8 @@ public class MainActivity extends AppCompatActivity {
     Bitmap musicSheet;
     String ba1;
     //public static String destination = "http://35.232.70.229/";
-    //public static String destination = "https://run.mocky.io/v3/b23ad42a-453f-4ce2-a570-dc1cc8e8fe24";
-    public static String destination = "https://run.mocky.io/v3/905b9664-0425-4bdd-8742-b6a77aca9461";
-
-
-    //private String selectedImagePath;
-    //private String fileManagerString;
+    public static String destination = "https://httpbin.org/get";
+    //public static String destination = "https://run.mocky.io/v3/905b9664-0425-4bdd-8742-b6a77aca9461";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,51 +80,22 @@ public class MainActivity extends AppCompatActivity {
         IVPreviewImage = findViewById(R.id.imageView);
     }
 
-    public void testMethod(View view){
+    public void testMethod(View view) {
         Intent intent = new Intent(this, TestActivity.class);
         startActivity(intent);
     }
 
     //Similar with the code that uses camera, the tutorial from which our code heavily referred is https://developer.android.com/training/camera/photobasics
-    public void takePicture(View view){
+    public void takePicture(View view) {
         Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            //We follwed an onlne tutorial and most of the code was taken from that tutorial, link: https://developer.android.com/training/basics/firstapp
-        }
+        startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
     }
 
     public void pickPhoto(View view) {
         Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
         startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_PHOTO_CODE);
-        /*
-        if (galleryIntent.resolveActivity(getPackageManager()) != null) {
-            //startActivityForResult(galleryIntent, PICK_PHOTO_CODE);
-            startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_PHOTO_CODE);
-        }
-        */
     }
-/*
-    public Bitmap loadFromUri(Uri photoUri) {
-        Bitmap image = null;
-        try {
-            // check version of Android on device
-            if(Build.VERSION.SDK_INT > 27){
-                // on newer versions of Android, use the new decodeBitmap method
-                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
-                image = ImageDecoder.decodeBitmap(source);
-            } else {
-                // support older versions of Android by using getBitmap
-                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
-*/
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -122,39 +103,104 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //ImageView imageView = (ImageView)findViewById(R.id.imageView);
             IVPreviewImage.setImageBitmap(imageBitmap);
         } else if (requestCode == PICK_PHOTO_CODE && resultCode == RESULT_OK) {
             selectedImage = data.getData();
-            //Bundle extras = data.getExtras();
-            //musicSheet = (Bitmap) data.getExtras().get("data");
-
+            try {
+                //getting bitmap object from uri
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                //displaying selected image to imageview
+                IVPreviewImage.setImageBitmap(bitmap);
+                //calling the method uploadBitmap to upload image
+                //uploadBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //Get path information
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
             Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
             cursor.moveToFirst();
 
             imagePath = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
-
-            //Bitmap photo = (Bitmap) data.getExtras().get("data");
-            //ImageView imageView = (ImageView)findViewById(R.id.imageView);
-            //imageView.setImageBitmap(photo);
-            if (selectedImage != null) {
-                IVPreviewImage.setImageURI(selectedImage);
-            }
-
         }
     }
 
     public void sendRequest(View view) {
         mRequestQueue = Volley.newRequestQueue(this);
-        mStringRequest = new StringRequest(Request.Method.GET, destination, new Response.Listener<String>(){
+        mStringRequest = new StringRequest(Request.Method.GET, destination, new Response.Listener<String>() {
 
             @Override
             public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();
-            }
-        }, error -> Log.i(MainActivity.class.getName(),"Error :" + error.toString()));
+            Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();
+        }
+    }, error -> Log.i(MainActivity.class.getName(), "Error :" + error.toString()));
         mRequestQueue.add(mStringRequest);
+}
+
+    /*
+     * The method is takes an image in Bitmap form and
+     * then it will return the byte[] array for the given bitmap.
+     * This basically prepares the image to be sent to the server
+     * */
+    public byte[] getFileDataBitMap(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
+
+    //UNTESTED
+    private void uploadBitmap(final Bitmap bitmap) {
+
+        //tag for the uploaded image - doesn't matter for now
+        String tags = "Music Sheet";
+
+        //custom volley request
+        UploadToServer volleyMultipartRequest = new UploadToServer(Request.Method.POST, destination,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("tags", tags);
+                return params;
+            }
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, UploadToServer.DataPart> getByteData() {
+                Map<String, UploadToServer.DataPart> params = new HashMap<>();
+                long imageName = System.currentTimeMillis();
+                params.put("pic", new UploadToServer.DataPart(imageName + ".png", getFileDataBitMap(bitmap)));
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+
 
 }
--- app/src/main/java/com/example/musicscoreapp/UploadToServer.java
+++ app/src/main/java/com/example/musicscoreapp/UploadToServer.java
@@ -1,49 +1,219 @@
 package com.example.musicscoreapp;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.util.Log;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+/**
+ * Credits to Belal for this custom multipart library to ease the image uploading
+ * https://www.simplifiedcoding.net/upload-image-to-server/#Android-Upload-Image-to-Server-using-Volley
+ */
+
+public class UploadToServer extends Request<NetworkResponse> {
+
+    private final String twoHyphens = "--";
+    private final String lineEnd = "\r\n";
+    private final String boundary = "apiclient-" + System.currentTimeMillis();
+
+    private Response.Listener<NetworkResponse> mListener;
+    private Response.ErrorListener mErrorListener;
+    private Map<String, String> mHeaders;
 
 
-public class UploadToServer extends AsyncTask<Void, Void, String> {
+    public UploadToServer(int method, String url,
+                                  Response.Listener<NetworkResponse> listener,
+                                  Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.mListener = listener;
+        this.mErrorListener = errorListener;
+    }
+
     @Override
-    protected String doInBackground(Void... voids) {
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return (mHeaders != null) ? mHeaders : super.getHeaders();
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return "multipart/form-data;boundary=" + boundary;
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        try {
+            // populate text payload
+            Map<String, String> params = getParams();
+            if (params != null && params.size() > 0) {
+                textParse(dos, params, getParamsEncoding());
+            }
+
+            // populate data byte payload
+            Map<String, DataPart> data = getByteData();
+            if (data != null && data.size() > 0) {
+                dataParse(dos, data);
+            }
+
+            // close multipart form data after text and file data
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
-    //some (probably outdated) code from online depending on the apache library - however currently im trying to use Volley first
-    /*
-    private ProgressDialog pd = new ProgressDialog(MainActivity.this);
-    protected void onPreExecute() {
-        super.onPreExecute();
-        pd.setMessage("Wait image uploading!");
-        pd.show();
+
+    /**
+     * Custom method handle data payload.
+     *
+     * @return Map data part label with data byte
+     * @throws AuthFailureError
+     */
+    protected Map<String, DataPart> getByteData() throws AuthFailureError {
+        return null;
     }
 
     @Override
-    protected String doInBackground(Void... params) {
-
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair("base64", ba1));
-        nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
+    protected Response<NetworkResponse> parseNetworkResponse(NetworkResponse response) {
         try {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(URL);
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = httpclient.execute(httppost);
-            String st = EntityUtils.toString(response.getEntity());
-            Log.v("log_tag", "In the try Loop" + st);
-
+            return Response.success(
+                    response,
+                    HttpHeaderParser.parseCacheHeaders(response));
         } catch (Exception e) {
-            Log.v("log_tag", "Error in http connection " + e.toString());
+            return Response.error(new ParseError(e));
         }
-        return "Success";
-
     }
 
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        pd.hide();
-        pd.dismiss();
+    @Override
+    protected void deliverResponse(NetworkResponse response) {
+        mListener.onResponse(response);
     }
+
+    @Override
+    public void deliverError(VolleyError error) {
+        mErrorListener.onErrorResponse(error);
+    }
+
+    /**
+     * Parse string map into data output stream by key and value.
+     *
+     * @param dataOutputStream data output stream handle string parsing
+     * @param params           string inputs collection
+     * @param encoding         encode the inputs, default UTF-8
+     * @throws IOException
      */
+    private void textParse(DataOutputStream dataOutputStream, Map<String, String> params, String encoding) throws IOException {
+        try {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                buildTextPart(dataOutputStream, entry.getKey(), entry.getValue());
+            }
+        } catch (UnsupportedEncodingException uee) {
+            throw new RuntimeException("Encoding not supported: " + encoding, uee);
+        }
+    }
+
+    /**
+     * Parse data into data output stream.
+     *
+     * @param dataOutputStream data output stream handle file attachment
+     * @param data             loop through data
+     * @throws IOException
+     */
+    private void dataParse(DataOutputStream dataOutputStream, Map<String, DataPart> data) throws IOException {
+        for (Map.Entry<String, DataPart> entry : data.entrySet()) {
+            buildDataPart(dataOutputStream, entry.getValue(), entry.getKey());
+        }
+    }
+
+    /**
+     * Write string data into header and data output stream.
+     *
+     * @param dataOutputStream data output stream handle string parsing
+     * @param parameterName    name of input
+     * @param parameterValue   value of input
+     * @throws IOException
+     */
+    private void buildTextPart(DataOutputStream dataOutputStream, String parameterName, String parameterValue) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + parameterName + "\"" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+        dataOutputStream.writeBytes(parameterValue + lineEnd);
+    }
+
+    /**
+     * Write data file into header and data output stream.
+     *
+     * @param dataOutputStream data output stream handle data parsing
+     * @param dataFile         data byte as DataPart from collection
+     * @param inputName        name of data input
+     * @throws IOException
+     */
+    private void buildDataPart(DataOutputStream dataOutputStream, DataPart dataFile, String inputName) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" +
+                inputName + "\"; filename=\"" + dataFile.getFileName() + "\"" + lineEnd);
+        if (dataFile.getType() != null && !dataFile.getType().trim().isEmpty()) {
+            dataOutputStream.writeBytes("Content-Type: " + dataFile.getType() + lineEnd);
+        }
+        dataOutputStream.writeBytes(lineEnd);
+
+        ByteArrayInputStream fileInputStream = new ByteArrayInputStream(dataFile.getContent());
+        int bytesAvailable = fileInputStream.available();
+
+        int maxBufferSize = 1024 * 1024;
+        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        byte[] buffer = new byte[bufferSize];
+
+        int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            dataOutputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        dataOutputStream.writeBytes(lineEnd);
+    }
+
+    class DataPart {
+        private String fileName;
+        private byte[] content;
+        private String type;
+
+        public DataPart() {
+        }
+
+        DataPart(String name, byte[] data) {
+            fileName = name;
+            content = data;
+        }
+
+        String getFileName() {
+            return fileName;
+        }
+
+        byte[] getContent() {
+            return content;
+        }
+
+        String getType() {
+            return type;
+        }
+
+    }
 }
